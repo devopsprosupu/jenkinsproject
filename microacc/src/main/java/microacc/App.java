19926056bc15c7b2fbcb94bf6427dabd60cf549d
devopsprosupu <104550774+devopsprosupu@users.noreply.github.com>
2022-05-14 22:46:54 +0530

    Add files via upload

---
+++ microacc/src/main/java/microacc/App.java
@@ -0,0 +1,13 @@
+package microacc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
